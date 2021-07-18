2f360e0268931b7fb6cdd750f1b7d75e7294b5a4
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2021-07-18 16:20:10 +0200

    adding commnad model

---
+++ src/tpm/base/models/commands.model.ts
@@ -0,0 +1,18 @@
+export enum BASECOMMANDS {
+    NEW = 'new',
+    CREARE = 'create',
+    HELP = 'help',
+    VERSION = '--version',
+    INFO = 'info'
+}
+
+export enum SECONDARY {
+    EXTENSION = 'extension',
+    TEMPLATE = 'template'
+}
+
+export enum FLAGS {
+    T = '-t',
+    F = '-f',
+    
+}
